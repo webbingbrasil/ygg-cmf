78c22e3cf4a8056b66e150d4a234fd48ac61681f
Danilo Andrade <mandrade.danilo@gmail.com>
2019-05-04 16:14:26 -0300

    add a helper class to create resource list datatable columns layout.

---
+++ src/Layout/Resource/ResourceColumn.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Ygg\Layout\Resource;
+
+use Ygg\Layout\Element;
+
+/**
+ * Class ResourceColumn
+ * @package Ygg\Layout\Resource
+ */
+class ResourceColumn implements Element
+{
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var int
+     */
+    protected $size;
+
+    /**
+     * @var int|null
+     */
+    protected $sizeXS;
+
+    /**
+     * @var boolean
+     */
+    protected $hideOnXs = false;
+
+    /**
+     * @param string   $key
+     * @param int      $size
+     * @param int|null $sizeXS
+     */
+    public function __construct(string $key, int $size, $sizeXS = null)
+    {
+        parent::__construct($size);
+        $this->key = $key;
+        $this->sizeXS = $sizeXS ?: $size;
+    }
+
+    public function hideOnXs(): void
+    {
+        $this->hideOnXs = true;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'key' => $this->key,
+            'size' => $this->size,
+            'sizeXS' => $this->sizeXS,
+            'hideOnXS' => $this->hideOnXs,
+        ];
+    }
+}
