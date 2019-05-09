221671e6235e373371bcc45f0fccd423f4600ee5
Danilo Andrade <mandrade.danilo@gmail.com>
2019-05-09 14:19:41 -0300

    add a field list component

---
+++ src/Fields/AutocompleteListField.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ygg\Fields;
+
+use InvalidArgumentException;
+use Ygg\Fields\Formatters\AutocompleteListFormatter;
+
+/**
+ * Class AutocompleteListField
+ * @package Ygg\Fields
+ */
+class AutocompleteListField extends ListField
+{
+    /**
+     * @param string $key
+     * @return $this
+     */
+    public static function make(string $key): self
+    {
+        return new static($key, static::FIELD_TYPE, new AutocompleteListFormatter);
+    }
+
+    /**
+     * @param AbstractField $field
+     * @return $this
+     */
+    public function addItemField(AbstractField $field): self
+    {
+        if (!$field instanceof AutocompleteField) {
+            throw new InvalidArgumentException('AutocompleteList item can only contain one field, and it must be a YggFormAutocompleteField');
+        }
+
+        return $this->setItemField($field);
+    }
+
+    /**
+     * @param AutocompleteField $field
+     * @return static
+     */
+    public function setItemField(AutocompleteField $field): self
+    {
+        $this->itemFields = [$field];
+
+        return $this;
+    }
+
+    /**
+     * @return AutocompleteField
+     */
+    public function autocompleteField(): AutocompleteField
+    {
+        return $this->itemFields()[0];
+    }
+}
