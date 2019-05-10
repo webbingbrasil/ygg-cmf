b44162b864c3edbb94b487ecf63e8a61b0d38c1c
Danilo Andrade <mandrade.danilo@gmail.com>
2019-05-10 11:14:51 -0300

    update form tab and row layout classes

---
+++ src/Layout/Form/FormRow.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ygg\Layout\Form;
+
+use Closure;
+use Ygg\Layout\Element;
+
+/**
+ * Class FormColumn
+ * @package Ygg\Layout\Form
+ */
+class FormRow implements Element
+{
+    use HasFieldRows;
+
+    protected $size;
+
+    /**
+     * FormRow constructor.
+     * @param $size
+     */
+    public function __construct($size)
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * @param string       $legend
+     * @param Closure|null $callback
+     * @return $this
+     */
+    public function addFieldset(string $legend, Closure $callback = null): self
+    {
+        $fieldset = new Fieldset($legend);
+        return $this->addElement($fieldset, $callback);
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+                'size' => $this->size
+            ] + $this->fieldsToArray();
+    }
+}
---
+++ src/Layout/Form/Tab.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Ygg\Layout\Form;
+
+use Closure;
+use Ygg\Layout\Element;
+use Ygg\Layout\WithElements;
+
+/**
+ * Class Tab
+ * @package Ygg\Layout
+ */
+class Tab implements Element
+{
+    use WithElements;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @param string $title
+     */
+    public function __construct(string $title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @param int     $size
+     * @param Closure $callback
+     * @return $this
+     */
+    public function addColumn(int $size, Closure $callback): self
+    {
+        $element = new FormRow($size);
+        return $this->addElement($element, $callback);
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'title' => $this->title,
+            'columns' => collect($this->elements)->map(function (Element $element) {
+                return $element->toArray();
+            })->all()
+        ];
+    }
+}
