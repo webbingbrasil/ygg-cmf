3768eef5c73b5104026b83c6934f6e1a86b19bdc
Danilo <mandrade.danilo@gmail.com>
2019-07-30 14:39:03 -0300

    Update lang file

--- resources/lang/front/en/resource_list.php
+++ resources/lang/front/en/resource_list.php
@@ -2,7 +2,7 @@
 
 return [
     'empty_text' => 'No result found',
-    'filter.search_placeholder' => 'Rechercher',
+    'filter.search_placeholder' => 'Search',
     'actions.resource.label' => 'Actions',
     'actions.instance.label' => 'Actions',
 ];
