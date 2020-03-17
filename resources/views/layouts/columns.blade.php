d07c04bdb0bfbca559e661200224ef24d858f164
Danilo Andrade <mandrade.danilo@gmail.com>
2020-03-17 14:21:21 -0300

    fix: add condition to wrapper column

--- resources/views/layouts/columns.blade.php
+++ resources/views/layouts/columns.blade.php
@@ -1,7 +1,7 @@
 <div class="hbox hbox-auto-xs">
     @foreach($manyForms as $key => $column)
         <div class="hbox-col" style="overflow: initial">
-            <div class="vbox wrapper @if ($loop->first) pl-0 @endif @if ($loop->last) pr-0 @endif">
+            <div class="vbox @if($wrapper) wrapper @endif @if ($loop->first) pl-0 @endif @if ($loop->last) pr-0 @endif">
                 @foreach($column as $item)
                     {!! $item ?? '' !!}
                 @endforeach
