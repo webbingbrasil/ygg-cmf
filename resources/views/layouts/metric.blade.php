654994525ad861851ffb2885728885d5bf95c110
Danilo Andrade <mandrade.danilo@gmail.com>
2020-06-15 19:30:33 -0300

    fix: get metric value and diff using custom fields

--- resources/views/layouts/metric.blade.php
+++ resources/views/layouts/metric.blade.php
@@ -6,17 +6,17 @@
         @foreach($metrics as $key => $metric)
             <div class="col  @if(!$loop->last) b-r @endif">
                 <small class="text-muted block mb-1">{{ __($key) }}</small>
-                <p class="h4 mb-1 text-black font-thin">{{ $metric['keyValue'] }}</p>
+                <p class="h4 mb-1 text-black font-thin">{{ $metric[$keyValue] }}</p>
 
-                @isset($metric['keyDiff'])
-                    @if((float)$metric['keyDiff'] < 0)
-                        <small class="text-xs text-danger">{{ $metric['keyDiff'] }} % <i
-                                class="icon-arrow-down v-top"></i></small>
-                    @elseif((float)$metric['keyDiff'] == 0)
-                        <small class="text-xs text-muted">{{ $metric['keyDiff'] }} % <i class="icon-refresh v-top"></i></small>
+                @isset($metric[$keyDiff])
+                    @if((float)$metric[$keyDiff] < 0)
+                        <small class="text-xs text-danger">{{ $metric[$keyDiff] }} % <i
+                                    class="icon-arrow-down v-top"></i></small>
+                    @elseif((float)$metric[$keyDiff] == 0)
+                        <small class="text-xs text-muted">{{ $metric[$keyDiff] }} % <i class="icon-refresh v-top"></i></small>
                     @else
-                        <small class="text-xs text-success">{{ $metric['keyDiff'] }} % <i
-                                class="icon-arrow-up v-top"></i></small>
+                        <small class="text-xs text-success">{{ $metric[$keyDiff] }} % <i
+                                    class="icon-arrow-up v-top"></i></small>
                     @endif
                 @endisset
             </div>
