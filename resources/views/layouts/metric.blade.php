b417d97be8154af85962a04eef92c2bbdd4a5d84
Danilo Andrade <mandrade.danilo@gmail.com>
2020-06-22 19:17:56 -0300

    fix: add options to set icon, color and link

--- resources/views/layouts/metric.blade.php
+++ resources/views/layouts/metric.blade.php
@@ -5,20 +5,32 @@
     <div class="row padder-v">
         @foreach($metrics as $key => $metric)
             <div class="col  @if(!$loop->last) b-r @endif">
-                <small class="text-muted block mb-1">{{ __($key) }}</small>
-                <p class="h4 mb-1 text-black font-thin">{{ $metric[$keyValue] }}</p>
+                @if(isset($metric['link']))
+                    <a href="{{$metric['link']}}">
+                        @endif
+                        <small class="text-muted block mb-1">{{ __($key) }}</small>
+                        <p class="h4 mb-1 {{Arr::get($metric, 'color', 'text-black')}} font-thin">
+                            {{ $metric[$keyValue] }}
+                            @if(isset($metric['icon']))
+                                <i class="{{$metric['icon']}} v-bottom"></i>
+                            @endif
+                        </p>
 
-                @isset($metric[$keyDiff])
-                    @if((float)$metric[$keyDiff] < 0)
-                        <small class="text-xs text-danger">{{ $metric[$keyDiff] }} % <i
-                                    class="icon-arrow-down v-top"></i></small>
-                    @elseif((float)$metric[$keyDiff] == 0)
-                        <small class="text-xs text-muted">{{ $metric[$keyDiff] }} % <i class="icon-refresh v-top"></i></small>
-                    @else
-                        <small class="text-xs text-success">{{ $metric[$keyDiff] }} % <i
-                                    class="icon-arrow-up v-top"></i></small>
-                    @endif
-                @endisset
+                        @isset($metric[$keyDiff])
+                            @if((float)$metric[$keyDiff] < 0)
+                                <small class="text-xs text-danger">{{ $metric[$keyDiff] }} % <i
+                                            class="icon-arrow-down v-top"></i></small>
+                            @elseif((float)$metric[$keyDiff] == 0)
+                                <small class="text-xs text-muted">{{ $metric[$keyDiff] }} % <i class="icon-refresh v-top"></i></small>
+                            @else
+                                <small class="text-xs text-success">{{ $metric[$keyDiff] }} % <i
+                                            class="icon-arrow-up v-top"></i></small>
+                            @endif
+                        @endisset
+
+                        @if(isset($metric['link']))
+                    </a>
+                @endif
             </div>
         @endforeach
     </div>
